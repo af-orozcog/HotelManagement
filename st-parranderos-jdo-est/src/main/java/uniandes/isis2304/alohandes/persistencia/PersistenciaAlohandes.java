5bd71681dea6a44c3c1dbd4a6562c984b509d864
nf.abondano <nf.abondano@uniandes.edu.co>
2020-03-24 09:11:39 -0500

    xd

--- st-parranderos-jdo-est/src/main/java/uniandes/isis2304/alohandes/persistencia/PersistenciaAlohandes.java
+++ st-parranderos-jdo-est/src/main/java/uniandes/isis2304/alohandes/persistencia/PersistenciaAlohandes.java
@@ -1913,7 +1913,7 @@ public class PersistenciaAlohandes
 	}
 	
 	public long reqc1Corrido(long idOperador) {
-		return reqc1.gananciasAñoActual(pmf.getPersistenceManager(), idOperador);
+		return reqc1.gananciasAñoCorrido(pmf.getPersistenceManager(), idOperador);
 	}
 	
 	public List<Oferta> reqc2(){
