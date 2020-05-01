2916e77ac0128a2c03b295ca9da3346c7dac4ec2
nf.abondano <nf.abondano@uniandes.edu.co>
2020-05-01 18:36:23 -0500

    Auto stash before merge of "master" and "origin/master"

--- Alohandes/src/main/java/uniandes/isis2304/alohandes/persistencia/SQLOferta.java
+++ Alohandes/src/main/java/uniandes/isis2304/alohandes/persistencia/SQLOferta.java
@@ -134,7 +134,11 @@ class SQLOferta
 		for (String servicio : lista) {
 			Query q = pm.newQuery(SQL, "SELECT DISTINCT o.id, o.precio, o.periodo, o.vivienda, o.fechainicio, o.fechafin "
 					+ "FROM " + pa.darTablaOferta ()+" o, " + pa.darTablaIncluye() + " i, " + pa.darTablaServicio() + "s "
-					+ "WHERE o.id = i.oferta AND i.servicio = s.id AND s.nombre <> ?");
+					+ "WHERE o.id = i.oferta AND i.servicio = s.id AND s.nombre <> ?"
+					+ " AND fechaInicio > ? AND fechaFin < ? AND habilitado = 1 AND o.id NOT IN"
+					+ "( SELECT o.id FROM "+ pa.darTablaOferta() + " o, " + pa.darTablaReserva()+ " r"
+					+" WHERE o.id = r.oferta AND ((r.inicio < ?  AND r.inicio > ?) OR (r.inicio > ? AND r.fin < ?))"
+					+ ")");
 			q.setResultClass(Oferta.class);
 			q.setParameters(servicio);
 			List<Oferta> eliminar = q.executeList();
@@ -152,15 +156,18 @@ public List<Oferta> darOfertasConServiciosYTipo(PersistenceManager pm, ArrayList
 		for (String servicio : lista) {
 			Query q = pm.newQuery(SQL, "SELECT DISTINCT o.id, o.precio, o.periodo, o.vivienda, o.fechainicio, o.fechafin "
 					+ "o.habilitada FROM " + pa.darTablaOferta ()+" o, " + pa.darTablaIncluye() + " i, " + pa.darTablaServicio() + "s, " + pa.darTablaVivienda() + "v, " + pa.darTablaOperador() + "op "
-					+ "WHERE o.id = i.oferta AND i.servicio = s.id AND s.nombre <> ? AND op.tipo_operador <> "+tipo+" AND op.periodo <> " + periodo);
+					+ "WHERE o.id = i.oferta AND i.servicio = s.id AND s.nombre <> ? AND op.tipo_operador <> ? AND op.periodo <> ?"
+					+ " AND fechaInicio > ? AND fechaFin < ? AND habilitado = 1 AND o.id NOT IN"
+					+ "( SELECT o.id FROM "+ pa.darTablaOferta() + " o, " + pa.darTablaReserva()+ " r"
+					+" WHERE o.id = r.oferta AND ((r.inicio < ?  AND r.inicio > ?) OR (r.inicio > ? AND r.fin < ?))"
+					+ ")");
 			q.setResultClass(Oferta.class);
-			q.setParameters(servicio);
+			q.setParameters(servicio, tipo, periodo );
 			List<Oferta> eliminar = q.executeList();
 			for (Oferta el : eliminar) {
 				ofertas.remove(el);
 			}
 		}
-		
 		return ofertas;
 	}
 
